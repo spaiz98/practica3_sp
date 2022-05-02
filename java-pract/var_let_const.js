5d51d29030e0718c27131824ab6c88e21d8107ad
spaiz98 <aldipa2016@hotmail.com>
2022-05-02 19:22:35 -0300

    pract java

---
+++ java-pract/var_let_const.js
@@ -0,0 +1,45 @@
+// variables
+
+var apellido = 'Paiz';
+var apellido = 'Alfonzo';
+console.log(apellido);
+
+var numero = 0;
+
+if(numero == 0);
+{
+    var apellido = 'Lopez';
+    console.log(apellido);
+}
+
+// constantes
+
+const valor = 5;
+const miarray= [1,10,100,1000];
+
+miarray.push(8);
+console.log(miarray);
+
+// funciones
+
+function sumar (num1, num2) {
+    console.log(num1 + num2)
+  }
+  sumar(10, 20)
+  
+  const sumarDos = (num1, num2) => {
+    console.log(num1 + num2)
+  }
+  sumarDos(10, 50)
+
+  // mostrar resultado
+
+  const sumar1 = (num1, num2) => {
+    return 'la suma es: ' + (num1 + num2)
+  }
+ 
+  let resultado = sumar1(10, 20)
+  console.log(resultado)
+  
+  
+ 
